7657693d785b98a2cde356a5268b44489c481d07
zehudzen <yadullahkurt@gmail.com>
2023-05-02 16:49:56 +0300

    ilk react proje kodları

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+       <img src="/logo192.png"></img>
+       <img src={logo} alt="" />
+       
+       
+       
+
+       
+
+    </div>
+  );
+}   
+
+export default App;
+
